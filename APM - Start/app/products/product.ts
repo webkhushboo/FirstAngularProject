276128d6d361b6bfc983f5c66ecf22d2e4a2a21b
Khushboo Agrawal <khagrawa@adobe.com>
2017-06-13 15:10:04 +0530

    adding product list component

---
+++ APM - Start/app/products/product.ts
@@ -0,0 +1,10 @@
+export interface IProduct {
+    productId : number,
+    productCode : string,
+    productName : string,
+    description :string,
+    releaseDate : string,
+    imageUrl : string,
+    price : number,
+    starRating : number
+}
