ee2424a29a0c3127e6f3971076c119e0d0c61fdc
Khushboo Agrawal <web.khushboo@gmail.com>
2017-06-15 22:57:01 +0530

    added service class

--- APM - Start/app/app.component.ts
+++ APM - Start/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import {ProductService} from './products/product.service';
 
 @Component ({
     selector : 'pm-app',
     template :' <div> <h1> {{pageTitle}} </h1>'+
     '<pm-products></pm-products>'+
-    '</div>'
+    '</div>',
+    providers : [ProductService]
 })
 
 export class AppComponent {
