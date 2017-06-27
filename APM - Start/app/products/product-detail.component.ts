4f769d9cc90278f2952e6a001953b9200eedeca2
Khushboo Agrawal <web.khushboo@gmail.com>
2017-06-27 22:14:17 +0530

    Added navigation and routing with parameter

--- APM - Start/app/products/product-detail.component.ts
+++ APM - Start/app/products/product-detail.component.ts
@@ -1,4 +1,5 @@
-import {Component} from '@angular/core';
+import {Component ,OnInit} from '@angular/core';
+import {ActivatedRoute ,Router} from '@angular/router';
 
 import {IProduct} from './product';
 
@@ -6,7 +7,20 @@ import {IProduct} from './product';
     templateUrl :'app/products/product-detail.component.html'
 })
 
-export class ProductDetailComponent {
+export class ProductDetailComponent implements OnInit{
     pageTitle : string ='Product Detail';
     product :IProduct;
+
+    constructor(private _route : ActivatedRoute,
+            private _router : Router){
+    }
+
+    ngOnInit() : void {
+        let id = +this._route.snapshot.params['id'];
+        this.pageTitle += `: ${id}`;
+    }
+
+    onBack() : void {
+        this._router.navigate(['/products']);
+    }
 }
